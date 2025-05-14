5fdd304c3472a3d11abcadb84600f3953f8ba000
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-05-14 18:26:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, jsonify, Response
 app = Flask(__name__)
 
 # ✅ Updated URL and Headers
-BASE_URL = "https://old-gods.hash96t14.workers.dev/1743080892102/cat/TV/1/"
+BASE_URL = "https://old-gods.hashjhc9s.workers.dev/1747313783014/cat/TV/1/"
 COOKIES = {'hashhackers_1337x_web_app': 'QBcphs7Xe/KJWn1RnYQNlQ=='}
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
